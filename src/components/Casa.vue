0d96dc0ac1e1d68c2e52d81c83c3173861d75fea
Yakson Chan <yaksonchan@blumar.com.br>
2022-04-30 14:46:49 -0300

    Commit inicial

---
+++ src/components/Casa.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    props: {
+        coord: String,
+        linha: Number,
+        coluna: Number,
+        pecaAtual: Object
+    },
+    computed: {
+        getClass(){
+            if(this.linha + this.coluna % 2 == 0)
+                return "branco";
+            return "preto";
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .branco {
+        background-color: #FFFFFF;
+    }
+    .preto {
+        background-color: #000000;
+    }
+</style>
